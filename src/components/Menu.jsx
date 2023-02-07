76c0eb79c5e81717c86e5b3cd5070549cdb92768
jayden-woo <juncheng1301@gmail.com>
2023-02-08 07:06:16 +1100

    Updated menu design and implemented algorithms minimizing on types

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Collapse,
-  CssBaseline,
   Divider,
   FormControl,
   FormControlLabel,
@@ -38,7 +37,7 @@ import { setDiagonalTraversal, setHeuristic, setPathAlgorithm } from "../feature
 const Menu = () => {
   const dispatch = useDispatch();
   const { appState, animating } = useSelector((store) => store.app);
-  const { selectedAlgo } = useSelector((store) => store.menu);
+  const { selectedAlgo, algoType } = useSelector((store) => store.menu);
   const { algorithm, heuristic, diagonal } = useSelector((store) => store.pathfinding);
   // Check for states when changes in the algorithms and heuristics are not allowed
   const disableChanges = animating || appState === APP_STATE.PAUSED;
@@ -106,13 +105,36 @@ const Menu = () => {
 
   return (
     <Box sx={{ width: DRAWER_WIDTH }}>
-      <CssBaseline />
       <Toolbar />
+      <FormGroup sx={{ pt: 2, pl: 2 }}>
+        <FormControlLabel
+          control={
+            <Switch
+              disabled={disableChanges}
+              onChange={(e) => dispatch(setDiagonalTraversal(e.target.checked))}
+            />
+          }
+          label="Diagonal Movement"
+        />
+      </FormGroup>
+      <FormGroup sx={{ py: 1, pl: 2 }}>
+        <FormControlLabel
+          control={
+            <Switch
+              disabled={disableChanges}
+              onChange={(e) => dispatch(updateMazeAnimation(!e.target.checked))}
+            />
+          }
+          label="Instant Maze"
+        />
+      </FormGroup>
+      <Divider />
       <List
         component="nav"
         subheader={<ListSubheader component="div">Animation Delay (ms)</ListSubheader>}
       >
         <Slider
+          size="small"
           sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, width: SLIDER_WIDTH, ml: 2 }}
           defaultValue={DELAY.DEFAULT}
           step={DELAY.STEP}
@@ -125,136 +147,142 @@ const Menu = () => {
       <Divider />
       <List
         component="nav"
-        subheader={<ListSubheader component="div">Path Traversal</ListSubheader>}
-      >
-        <FormGroup sx={{ pl: 2 }}>
-          <FormControlLabel
-            control={
-              <Switch
-                disabled={disableChanges}
-                onChange={(e) => dispatch(setDiagonalTraversal(e.target.checked))}
-              />
+        subheader={
+          <ListSubheader
+            component="button"
+            sx={{
+              ":enabled": { cursor: "pointer" },
+              py: 0,
+              width: "100%",
+              border: "none",
+              textAlign: "start",
+            }}
+            disabled={disableChanges}
+            onClick={() =>
+              algoType !== ALGORITHM_TYPES.PATHFINDING &&
+              handleAlgoClick(PATH_ALGORITHMS.DEPTH_FIRST_SEARCH)
             }
-            label="Allow Diagonals"
-          />
-        </FormGroup>
+          >
+            Search Algorithms
+          </ListSubheader>
+        }
+      >
+        <Collapse in={algoType === ALGORITHM_TYPES.PATHFINDING} timeout="auto" unmountOnExit>
+          <ListItemButton
+            selected={selectedAlgo === PATH_ALGORITHMS.DEPTH_FIRST_SEARCH}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(PATH_ALGORITHMS.DEPTH_FIRST_SEARCH)}
+          >
+            <ListItemText primary="Depth-First Search" />
+          </ListItemButton>
+          <ListItemButton
+            selected={selectedAlgo === PATH_ALGORITHMS.BREADTH_FIRST_SEARCH}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(PATH_ALGORITHMS.BREADTH_FIRST_SEARCH)}
+          >
+            <ListItemText primary="Breadth-First Search" />
+          </ListItemButton>
+          <ListItemButton
+            sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}
+            selected={selectedAlgo === PATH_ALGORITHMS.GREEDY_BEST_FIRST_SEARCH}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(PATH_ALGORITHMS.GREEDY_BEST_FIRST_SEARCH)}
+          >
+            <ListItemText primary="Greedy Best-First Search" />
+            <Collapse
+              in={selectedAlgo === PATH_ALGORITHMS.GREEDY_BEST_FIRST_SEARCH}
+              timeout="auto"
+              unmountOnExit
+            >
+              {heuristics}
+            </Collapse>
+          </ListItemButton>
+          <ListItemButton
+            sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}
+            selected={selectedAlgo === PATH_ALGORITHMS.A_STAR_ALGORITHM}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(PATH_ALGORITHMS.A_STAR_ALGORITHM)}
+          >
+            <ListItemText primary="A* Algorithm" />
+            <Collapse
+              in={selectedAlgo === PATH_ALGORITHMS.A_STAR_ALGORITHM}
+              timeout="auto"
+              unmountOnExit
+            >
+              {heuristics}
+            </Collapse>
+          </ListItemButton>
+          <ListItemButton
+            selected={selectedAlgo === PATH_ALGORITHMS.DIJKSTRA_ALGORITHM}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(PATH_ALGORITHMS.DIJKSTRA_ALGORITHM)}
+          >
+            <ListItemText primary="Dijkstra's Algorithm" />
+          </ListItemButton>
+        </Collapse>
       </List>
       <Divider />
       <List
         component="nav"
-        subheader={<ListSubheader component="div">Maze Generation</ListSubheader>}
-      >
-        <FormGroup sx={{ pl: 2 }}>
-          <FormControlLabel
-            control={
-              <Switch
-                defaultChecked
-                disabled={disableChanges}
-                onChange={(e) => dispatch(updateMazeAnimation(e.target.checked))}
-              />
+        subheader={
+          <ListSubheader
+            component="button"
+            sx={{
+              ":enabled": { cursor: "pointer" },
+              py: 0,
+              width: "100%",
+              border: "none",
+              textAlign: "start",
+            }}
+            disabled={disableChanges}
+            onClick={() =>
+              algoType !== ALGORITHM_TYPES.GENERATION &&
+              handleAlgoClick(MAZE_ALGORITHMS.BASIC_RANDOM)
             }
-            label="Animate Maze"
-          />
-        </FormGroup>
+          >
+            Maze Generation Algorithms
+          </ListSubheader>
+        }
+      >
+        <Collapse in={algoType === ALGORITHM_TYPES.GENERATION} timeout="auto" unmountOnExit>
+          <ListItemButton
+            selected={selectedAlgo === MAZE_ALGORITHMS.BASIC_RANDOM}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(MAZE_ALGORITHMS.BASIC_RANDOM)}
+          >
+            <ListItemText primary="Basic Random" />
+          </ListItemButton>
+          <ListItemButton
+            selected={selectedAlgo === MAZE_ALGORITHMS.RECURSIVE_BACKTRACKING}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(MAZE_ALGORITHMS.RECURSIVE_BACKTRACKING)}
+          >
+            <ListItemText primary="Recursive Backtracking" />
+          </ListItemButton>
+          <ListItemButton
+            selected={selectedAlgo === MAZE_ALGORITHMS.RECURSIVE_DIVISION}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(MAZE_ALGORITHMS.RECURSIVE_DIVISION)}
+          >
+            <ListItemText primary="Recursive Division" />
+          </ListItemButton>
+          <ListItemButton
+            selected={selectedAlgo === MAZE_ALGORITHMS.KRUSKAL_ALGORITHM}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(MAZE_ALGORITHMS.KRUSKAL_ALGORITHM)}
+          >
+            <ListItemText primary="Kruskal's Algorithm" />
+          </ListItemButton>
+          <ListItemButton
+            selected={selectedAlgo === MAZE_ALGORITHMS.PRIM_ALGORITHM}
+            disabled={disableChanges}
+            onClick={() => handleAlgoClick(MAZE_ALGORITHMS.PRIM_ALGORITHM)}
+          >
+            <ListItemText primary="Prim's Algorithm" />
+          </ListItemButton>
+        </Collapse>
       </List>
       <Divider />
-      <List
-        component="nav"
-        subheader={<ListSubheader component="div">Search Algorithms</ListSubheader>}
-      >
-        <ListItemButton
-          selected={selectedAlgo === PATH_ALGORITHMS.DEPTH_FIRST_SEARCH}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(PATH_ALGORITHMS.DEPTH_FIRST_SEARCH)}
-        >
-          <ListItemText primary="Depth-First Search" />
-        </ListItemButton>
-        <ListItemButton
-          selected={selectedAlgo === PATH_ALGORITHMS.BREADTH_FIRST_SEARCH}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(PATH_ALGORITHMS.BREADTH_FIRST_SEARCH)}
-        >
-          <ListItemText primary="Breadth-First Search" />
-        </ListItemButton>
-        <ListItemButton
-          sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}
-          selected={selectedAlgo === PATH_ALGORITHMS.GREEDY_BEST_FIRST_SEARCH}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(PATH_ALGORITHMS.GREEDY_BEST_FIRST_SEARCH)}
-        >
-          <ListItemText primary="Greedy Best-First Search" />
-          <Collapse
-            in={selectedAlgo === PATH_ALGORITHMS.GREEDY_BEST_FIRST_SEARCH}
-            timeout="auto"
-            unmountOnExit
-          >
-            {heuristics}
-          </Collapse>
-        </ListItemButton>
-        <ListItemButton
-          sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}
-          selected={selectedAlgo === PATH_ALGORITHMS.A_STAR_ALGORITHM}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(PATH_ALGORITHMS.A_STAR_ALGORITHM)}
-        >
-          <ListItemText primary="A* Algorithm" />
-          <Collapse
-            in={selectedAlgo === PATH_ALGORITHMS.A_STAR_ALGORITHM}
-            timeout="auto"
-            unmountOnExit
-          >
-            {heuristics}
-          </Collapse>
-        </ListItemButton>
-        <ListItemButton
-          selected={selectedAlgo === PATH_ALGORITHMS.DIJKSTRA_ALGORITHM}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(PATH_ALGORITHMS.DIJKSTRA_ALGORITHM)}
-        >
-          <ListItemText primary="Dijkstra's Algorithm" />
-        </ListItemButton>
-      </List>
-      <Divider />
-      <List
-        component="nav"
-        subheader={<ListSubheader component="div">Maze Generation Algorithms</ListSubheader>}
-      >
-        <ListItemButton
-          selected={selectedAlgo === MAZE_ALGORITHMS.BASIC_RANDOM}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(MAZE_ALGORITHMS.BASIC_RANDOM)}
-        >
-          <ListItemText primary="Basic Random" />
-        </ListItemButton>
-        <ListItemButton
-          selected={selectedAlgo === MAZE_ALGORITHMS.RECURSIVE_BACKTRACKING}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(MAZE_ALGORITHMS.RECURSIVE_BACKTRACKING)}
-        >
-          <ListItemText primary="Recursive Backtracking" />
-        </ListItemButton>
-        <ListItemButton
-          selected={selectedAlgo === MAZE_ALGORITHMS.RECURSIVE_DIVISION}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(MAZE_ALGORITHMS.RECURSIVE_DIVISION)}
-        >
-          <ListItemText primary="Recursive Division" />
-        </ListItemButton>
-        <ListItemButton
-          selected={selectedAlgo === MAZE_ALGORITHMS.KRUSKAL_ALGORITHM}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(MAZE_ALGORITHMS.KRUSKAL_ALGORITHM)}
-        >
-          <ListItemText primary="Kruskal's Algorithm" />
-        </ListItemButton>
-        <ListItemButton
-          selected={selectedAlgo === MAZE_ALGORITHMS.PRIM_ALGORITHM}
-          disabled={disableChanges}
-          onClick={() => handleAlgoClick(MAZE_ALGORITHMS.PRIM_ALGORITHM)}
-        >
-          <ListItemText primary="Prim's Algorithm" />
-        </ListItemButton>
-      </List>
     </Box>
   );
 };
